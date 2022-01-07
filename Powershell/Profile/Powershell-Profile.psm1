3094bf07530d5b8fe4d72fe33fa2348e7f20967f
idsec-inc <mathias.dumont@mdtspace.net>
2022-01-07 08:45:07 -0500

    commit

--- Powershell/Profile/Powershell-Profile.psm1
+++ Powershell/Profile/Powershell-Profile.psm1
@@ -1,18 +1,18 @@
 Function LoadProfile {
-<#
-.SYNOPSIS
-Offering a tab to select which profile do you want to use.
+    <#
+    .SYNOPSIS
+    Offering a tab to select which profile do you want to use.
 
-.DESCRIPTION
-Offering a tab to select which profile do you want to use.
-To load different profile or switch profile
+    .DESCRIPTION
+    Offering a tab to select which profile do you want to use.
+    To load different profile or switch profile
 
-.EXAMPLE
-PS> LoadProfile
+    .EXAMPLE
+    PS> LoadProfile
 
-.NOTES
-....
-#>
+    .NOTES
+    ....
+    #>
     Try{
         stop-transcript|out-null
       }
@@ -83,53 +83,177 @@ Session démarrée à : $(get-date)
 }
 
 Function startTranscript() {
-<#
-.SYNOPSIS
-Start transcript.
+    <#
+    .SYNOPSIS
+    Start transcript.
 
-.DESCRIPTION
-Start transcript.
-Save datas to transcript file
+    .DESCRIPTION
+    Start transcript.
+    Save datas to transcript file
 
-.EXAMPLE
-PS> startTranscript
+    .EXAMPLE
+    PS> startTranscript
 
-.NOTES
-....
-#>
+    .NOTES
+    ....
+    #>
     $dte = (get-date).tostring()
     $dte = $dte -replace "[:\s/]","."
     start-transcript -path "$TranscriptFolder\$($SelectedProfile.TenantName)-$dte.txt"
 }
+
+Function stopTranscript() {
+    <#
+    .SYNOPSIS
+    Stop transcript
+    
+    .DESCRIPTION
+    stop-transcript
+    
+    .EXAMPLE
+    PS> StopTranscript
+    
+    .NOTES
+    ....
+    #>
+        stop-Transcript
+    }
+#===================
+#    Connection to module
+#===================
 Function ConnectAAD {
-<#
-.SYNOPSIS
-Connect to Azure AD with UPN included in json file
+    <#
+    .SYNOPSIS
+    Connect to Azure AD with UPN included in json file
 
-.DESCRIPTION
-Connect to Azure AD with UPN included in json file
+    .DESCRIPTION
+    Connect to Azure AD with UPN included in json file
 
-.EXAMPLE
-PS> Connect-AzureAD -AccountId $($SelectedProfile.Account)
+    .EXAMPLE
+    PS> Connect-AzureAD -AccountId $($SelectedProfile.Account)
 
-.NOTES
-....
-#>
+    .NOTES
+    ....
+    #>
     Connect-AzureAD -AccountId $($SelectedProfile.Account)
 }
-Function stopTranscript() {
-<#
-.SYNOPSIS
-Stop transcript
 
-.DESCRIPTION
-stop-transcript
+Function ConnectEXO() {
+    <#
+    .SYNOPSIS
+    Connect to ExchangeOnline with UPN included in json file
 
-.EXAMPLE
-PS> StopTranscript
+    .DESCRIPTION
+    Connect to ExchangeOnline with UPN included in json file
 
-.NOTES
-....
+    .EXAMPLE
+    PS> Connect-ExchangeOnline -UserPrincipalName $($SelectedProfile.Account)
+
+    .NOTES
+    ....
+    #>
+    Connect-ExchangeOnline -UserPrincipalName $($SelectedProfile.Account)
+}
+
+Function DisconnectEXO() {
+    <#
+    .SYNOPSIS
+    Disconnect to ExchangeOnline
+    
+    .DESCRIPTION
+    Disconnect to ExchangeOnline
+    
+    .EXAMPLE
+    PS> Disconnect-ExchangeOnline
+    
+    .NOTES
+    ....
+    #>
+        Disconnect-ExchangeOnline
+}
+
+#===================
+#    Exchange Online
+#===================
+Function Get-ModernAuth() {
+    <#
+    .SYNOPSIS
+    Disconnect to ExchangeOnline
+    
+    .DESCRIPTION
+    Disconnect to ExchangeOnline
+    
+    .EXAMPLE
+    PS> Disconnect-ExchangeOnline
+    
+    .NOTES
+    Source: https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/enable-or-disable-modern-authentication-in-exchange-online
+    #>
+    $Status = $(Get-OrganizationConfig).OAuth2ClientProfileEnabled
+    Write-Host "Status: $Status" -ForegroundColor Yellow
+    If ($Status -eq $false) {
+        Write-Host "It's recommended to enable it ?"
+    }
+    # Enable Modern Auth (it's not a block access): Set-OrganizationConfig -OAuth2ClientProfileEnabled $true
+    # Disable Modern Auth (force Basic Auth): Set-OrganizationConfig -OAuth2ClientProfileEnabled $false
+}
+
+Function Get-AuthPolicies {
+    <#
+    .SYNOPSIS
+    Disconnect to ExchangeOnline
+    
+    .DESCRIPTION
+    Disconnect to ExchangeOnline
+    
+    .EXAMPLE
+    PS> Disconnect-ExchangeOnline
+    
+    .NOTES
+    Source: https://docs.microsoft.com/en-us/powershell/module/exchange/set-authenticationpolicy?view=exchange-ps
+    #>
+    $Status = $(Get-OrganizationConfig).DefaultAuthenticationPolicy
+    Get-AuthenticationPolicy | ft Name -Auto
+
+    # Create Auth Default Policy
+    #Set-AuthenticationPolicy -Identity "Block Basic Auth" -AllowBasicAuthPop -AllowBasicAuthImap:$false
+
+    # Set Defautl Policy to organization
+    Set-OrganizationConfig -DefaultAuthenticationPolicy "Block Basic Auth"
+}
+
+Function Disable-BasicAuth {
+    <#
+    .SYNOPSIS
+    Disconnect to ExchangeOnline
+    
+    .DESCRIPTION
+    Disconnect to ExchangeOnline
+    
+    .EXAMPLE
+    PS> Disconnect-ExchangeOnline
+    
+    .NOTES
+    # Source: https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/disable-basic-authentication-in-exchange-online
 #>
-    stop-Transcript
+
+
+}
+
+Function Disable-BasicAuth {
+    <#
+    .SYNOPSIS
+    Disconnect to ExchangeOnline
+
+    .DESCRIPTION
+    Disconnect to ExchangeOnline
+
+    .EXAMPLE
+    PS> Disconnect-ExchangeOnline
+
+    .NOTES
+    # Source: https://docs.microsoft.com/en-us/exchange/clients-and-mobile-in-exchange-online/authenticated-client-smtp-submission
+#>
+
+
 }

9c0b5d3ad71ea23d8c90567f610b8cb66eb46132
IdSec Inc <mathias.dumont@mdtspace.net>
2022-10-19 20:22:26 -0400

    commit

--- Device/Add-User-Admin.ps1
+++ Device/Add-User-Admin.ps1
@@ -5,7 +5,7 @@ Function AddMyAccountAdminLocal {
     If ($SpecificUpn) {
         $UPN = Read-Host "UserPrincipalName/Email:"
     } Else {
-        $UPN = whoami /UPN
+        $UPN = whoami /upn
     }
     net localgroup "Administrators" /add "AzureAD\$UPN"
 
